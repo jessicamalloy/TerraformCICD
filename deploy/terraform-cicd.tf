7577736f0f49e95a8254214479cbdba6ee780434
Jessica Malloy <jessica.malloy@alleninstitute.org>
2023-11-13 01:33:47 -0800

    adding example instance

--- deploy/terraform-cicd.tf
+++ deploy/terraform-cicd.tf
@@ -78,3 +78,8 @@ module "terraform_cicd_codepipeline" {
     module.terraform_cicd_db 
   ]
 }
+
+resource "aws_instance" "example" {
+  ami           =  "ami-0fb653ca2d3203ac1" 
+  instance_type = "t2.micro"
+}
